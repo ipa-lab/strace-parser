c20040a6ffdac3eb173b4dfc9e73495b1aa0305d
Michael Schröder <michael.schroeder@tuwien.ac.at>
2020-09-30 11:27:26 +0200

    Simplify syscall finishing

--- Strace/Finish.hs
+++ Strace/Finish.hs
@@ -1,50 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- | Pass to stitch together unfinished system calls.
 module Strace.Finish (finishSystemCalls) where
 
 import Data.Coerce
+import Data.Foldable
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
-import Strace.Types
 import Debug.Trace
-import Data.Foldable
+import Strace.Types
+
+type SysCallMap = IntMap (Line Event)
 
 finishSystemCalls :: Trace -> Trace
-finishSystemCalls = reverse . snd . foldl' go (emptyState, [])
-  where
-    go :: (State, Trace) -> Line Event -> (State, Trace)
-    go (s, ls) l@(Line (Just pid) time e) = traceShow l $ case e of
-      SystemCall (OtherSystemCall _ _ Unfinished) -> case suspend l s of
-        Left err -> error err -- TODO
-        Right s' -> (s', ls)
-      SystemCall (OtherSystemCall _ _ Resumed) -> case resume l s of
-        Left err -> error err -- TODO
-        Right (s', l') -> (s', l' : ls)
-      _ -> (s, l : ls)
-
-newtype State = State (IntMap (Line Event))
-  deriving (Show)
-
-emptyState :: State
-emptyState = State IntMap.empty
-
-suspend :: Line Event -> State -> Either String State
-suspend (Line Nothing _ _) _ = Left "unfinished syscall without PID"
-suspend l@(Line (Just pid) _ e) (State s) = case e of
-  SystemCall (OtherSystemCall name _ Unfinished) -> case IntMap.lookup (fromEnum pid) s of
-    Just _ -> Left $ "inconsistent trace at " ++ show l -- TODO: better msg
-    Nothing -> Right $ State $ IntMap.insert (fromEnum pid) l s
-  _ -> Left "impossible"
-
-resume :: Line Event -> State -> Either String (State, Line Event)
-resume (Line Nothing _ _) _ = Left "unfinished syscall without PID"
-resume l@(Line (Just pid) t2 e) (State s) = case e of
-  SystemCall (OtherSystemCall name2 args2 Resumed) -> case IntMap.lookup (fromEnum pid) s of
-    Nothing -> Left $ "inconsistent trace at " ++ show (l,s) -- TODO: better msg
-    Just (Line _ t1 (SystemCall (OtherSystemCall name1 args1 Unfinished)))
-      | name1 == name2 ->
-        let s' = State $ IntMap.delete (fromEnum pid) s
-            l' = Line (Just pid) t2 e' -- TODO: fix time
-            e' = SystemCall (OtherSystemCall name1 (args1 <> args2) Finished)
-         in Right (s', l')
-    _ -> Left $ "inconsistent trace at " ++ show l -- TODO: better msg
-  _ -> Left "impossible"
+finishSystemCalls = reverse . snd . foldl' go (mempty, [])
+  where    
+    go :: (SysCallMap, Trace) -> Line Event -> (SysCallMap, Trace)
+    go (m, ls) l@(Line (Just pid) t2 (SystemCall c2))
+      | OtherSystemCall _ _ Unfinished <- c2 =
+        let m' = IntMap.insert (fromEnum pid) l m in (m', ls)
+      | OtherSystemCall name2 args2 Resumed <- c2,
+        Just (Line _ t1 (SystemCall c1)) <- IntMap.lookup (fromEnum pid) m,
+        OtherSystemCall name1 args1 Unfinished <- c1,
+        name1 == name2 =
+        let m' = IntMap.delete (fromEnum pid) m
+            l' = Line (Just pid) t1 (SystemCall c') -- TODO: merge t1 and t2 ?
+            c' = OtherSystemCall name1 (args1 <> args2) Finished
+         in (m', l' : ls)
+    go (m, ls) l = (m, l : ls)
