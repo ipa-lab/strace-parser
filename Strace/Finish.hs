819559c0bb72c1e439ae43ef7080595464e69090
Michael Schröder <michael.schroeder@tuwien.ac.at>
2020-10-06 17:22:35 +0200

    Improve memory performance

--- Strace/Finish.hs
+++ Strace/Finish.hs
@@ -10,15 +10,20 @@ import qualified Data.IntMap.Strict as IntMap
 import Debug.Trace
 import Strace.Types
 
+data Pair a b = Pair !a !b
+
+get2 :: Pair a b -> b
+get2 (Pair _ b) = b
+
 type SysCallMap = IntMap Line
 
 finishSystemCalls :: Trace -> Trace
-finishSystemCalls = reverse . snd . foldl' go (mempty, [])
+finishSystemCalls = reverse . get2 . foldl' go (Pair mempty [])
   where    
-    go :: (SysCallMap, Trace) -> Line -> (SysCallMap, Trace)
-    go (m, ls) l@(Line pid t2 (SystemCall c2))
+    go :: Pair SysCallMap Trace -> Line -> Pair SysCallMap Trace
+    go (Pair m ls) l@(Line pid t2 (SystemCall c2))
       | OtherSystemCall _ _ Unfinished <- c2 =
-        let m' = IntMap.insert (fromEnum pid) l m in (m', ls)
+        let m' = IntMap.insert (fromEnum pid) l m in Pair m' ls
       | OtherSystemCall name2 args2 Resumed <- c2,
         Just (Line _ t1 (SystemCall c1)) <- IntMap.lookup (fromEnum pid) m,
         OtherSystemCall name1 args1 Unfinished <- c1,
@@ -26,5 +31,5 @@ finishSystemCalls = reverse . snd . foldl' go (mempty, [])
         let m' = IntMap.delete (fromEnum pid) m
             l' = Line pid t1 (SystemCall c') -- TODO: merge t1 and t2 ?
             c' = OtherSystemCall name1 (args1 <> args2) Finished
-         in (m', l' : ls)
-    go (m, ls) l = (m, l : ls)
+         in Pair m' (l' : ls)
+    go (Pair m ls) l = Pair m (l : ls)
